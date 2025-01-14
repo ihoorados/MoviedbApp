b9967207e6f69c8a417761906e1175b4d026c91f
Hoorad <hooradramezani@gmail.com>
2025-01-14 17:48:52 +0330

    extend Endpoint with make request variable

--- MoviedbApp/Infrastructure/EndPoint.swift
+++ MoviedbApp/Infrastructure/EndPoint.swift
@@ -25,3 +25,45 @@ enum ApiHTTPMethod: String {
     case DELETE
     case PATCH
 }
+
+extension Endpoint {
+    
+    var makeRequest: URLRequest {
+        
+        var urlComponents = URLComponents(string: baseURLString)
+        var longPath = baseURLString.last != "/" ? "/" : ""
+        
+        longPath.append(path)
+        urlComponents?.path = longPath
+
+        if let queryForCalls = queryForCall {
+            urlComponents?.queryItems = [URLQueryItem]()
+            for queryForCall in queryForCalls {
+                urlComponents?.queryItems?.append(URLQueryItem(name: queryForCall.name, value: queryForCall.value))
+            }
+        }
+
+        guard let url = urlComponents?.url else { return URLRequest(url: URL(string: baseURLString)!) }
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+
+        if let headers = headers {
+            for header in headers {
+                request.addValue(header.value, forHTTPHeaderField: header.key)
+            }
+        }
+        
+        if let params = params {
+
+            let jsonData = try? JSONSerialization.data(withJSONObject: params, options: [])
+            request.httpBody = jsonData
+        }
+
+        if let customDataBody = customDataBody {
+            request.httpBody = customDataBody
+        }
+        return request
+    }
+
+   
+}
