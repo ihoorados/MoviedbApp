909d83b23b988aa956c8da86c619251f8e204c63
Hoorad <hooradramezani@gmail.com>
2025-01-14 18:08:04 +0330

    implementation of MoviesSearchFlowCoordinator for start

--- MoviedbApp/Coordinator/MoviesSearchFlowCoordinator.swift
+++ MoviedbApp/Coordinator/MoviesSearchFlowCoordinator.swift
@@ -5,3 +5,25 @@
 //  Created by Hoorad on 1/14/25.
 //
 
+import UIKit
+
+protocol MoviesSearchCoordinator: AnyObject{}
+
+final class MoviesSearchFlowCoordinator: MoviesSearchCoordinator{
+    
+    private weak var navigationController: UINavigationController?
+    private weak var moviesQueriesSuggestionsVC: UIViewController?
+    private weak var rootVC: MoviesListViewController?
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+        let vc = MoviesListViewController()
+        navigationController?.pushViewController(vc, animated: false)
+        rootVC = vc
+    }
+
+}
