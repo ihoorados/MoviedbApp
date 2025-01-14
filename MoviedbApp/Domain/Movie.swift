a685f3c7e44b186a601db8f84b8d23adc3cd3625
Hoorad <hooradramezani@gmail.com>
2025-01-14 17:36:41 +0330

    implement movie domain structure

---
+++ MoviedbApp/Domain/Movie.swift
@@ -0,0 +1,25 @@
+//
+//  Movie.swift
+//  MoviedbApp
+//
+//  Created by Hoorad on 1/14/25.
+//
+
+import Foundation
+
+struct Movie: Equatable, Identifiable {
+    
+    typealias Identifier = String
+    let id: Identifier
+    let title: String?
+    let posterPath: String?
+    let overview: String?
+    let releaseDate: Date?
+}
+
+struct MoviesPage: Equatable {
+    
+    let page: Int
+    let totalPages: Int
+    let movies: [Movie]
+}
