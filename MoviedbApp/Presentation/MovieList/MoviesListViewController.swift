cab2e905c3fc31d95b7c509ad29d553a0f2e270e
Hoorad <hooradramezani@gmail.com>
2025-01-14 18:28:17 +0330

    adopt background for light and dark mode & add title

--- MoviedbApp/Presentation/MovieList/MoviesListViewController.swift
+++ MoviedbApp/Presentation/MovieList/MoviesListViewController.swift
@@ -13,17 +13,14 @@ class MoviesListViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.setupUIView()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUIView(){
+        
+        self.view.backgroundColor = .systemBackground
+        self.title = "Search Movie"
     }
-    */
 
 }
