fbd4c6c6ee2d2aae90ef38fe0631f255618057b9
Hoorad <hooradramezani@gmail.com>
2025-01-15 12:37:05 +0330

    add properties and Constant String

--- MoviedbApp/Presentation/MovieList/MoviesListViewModel.swift
+++ MoviedbApp/Presentation/MovieList/MoviesListViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Hoorad on 1/14/25.
 //
 
+import Combine
+
 final class MoviesListViewModel{
     
     
@@ -30,4 +32,18 @@ final class MoviesListViewModel{
         case none
     }
     
+    // MARK: Published Properties
+
+    @Published var items: [Movie] = []
+    @Published var state: MoviesListViewModelState = .none
+    @Published var query: String = ""
+    @Published var error: String = ""
+    
+    
+    // MARK: Public Properties
+    
+    var screenTitle: String { return "Movies" }
+    var centerTitle: String { return "Search results" }
+    var errorTitle: String { return "Error" }
+    var searchBarPlaceholder: String { return "Type something here to search" }
 }
