cce8dfc879d5678c0eab322b807cf779ecd87525
Hoorad <hooradramezani@gmail.com>
2025-01-15 22:32:35 +0330

    add connection error

---
+++ MoviedbApp/Utils/ConnectionError.swift
@@ -0,0 +1,21 @@
+//
+//  ConnectionError.swift
+//  MoviedbApp
+//
+//  Created by Hoorad on 1/15/25.
+//
+
+import Foundation
+
+protocol ConnectionError: Error {
+    var isInternetConnectionError: Bool { get }
+}
+
+extension Error {
+    var isInternetConnectionError: Bool {
+        guard let error = self as? ConnectionError, error.isInternetConnectionError else {
+            return false
+        }
+        return true
+    }
+}
