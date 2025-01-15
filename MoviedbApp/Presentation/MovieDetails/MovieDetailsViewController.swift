9640f439036ebb79ec18ab233be392d935cf4680
Hoorad <hooradramezani@gmail.com>
2025-01-15 20:14:48 +0330

    add UI Properties Needed

--- MoviedbApp/Presentation/MovieDetails/MovieDetailsViewController.swift
+++ MoviedbApp/Presentation/MovieDetails/MovieDetailsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class MovieDetailsViewController: UIViewController {
     
@@ -22,6 +23,59 @@ class MovieDetailsViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("Storyboard are a pain")
     }
+    
+    // MARK: UI Properties
+    
+    private let verticalStackView: UIStackView = {
+        
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+        stackView.backgroundColor = .clear
+        return stackView
+    }()
+    
+    private let movieImageView: UIImageView = {
+       
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+       
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 19, weight: .bold)
+        label.textColor = .label
+        return label
+    }()
+    
+    private let overViewTextView: UITextView = {
+       
+        let textView = UITextView()
+        textView.textColor = .secondaryLabel
+        textView.font = .systemFont(ofSize: 17, weight: .medium)
+        return textView
+    }()
+    
+    private let releaseDateLabel: UILabel = {
+       
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 15, weight: .medium)
+        return label
+    }()
+    
+    private let voteLabel: UILabel = {
+       
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 15, weight: .medium)
+        return label
+    }()
+
+    private var subscriptions = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
