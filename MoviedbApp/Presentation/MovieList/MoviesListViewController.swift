4064a56015e77d4ac188e6bdd2bb5ed11b4d462d
Hoorad <hooradramezani@gmail.com>
2025-01-15 16:08:28 +0330

    add list view controller dependency

--- MoviedbApp/Presentation/MovieList/MoviesListViewController.swift
+++ MoviedbApp/Presentation/MovieList/MoviesListViewController.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 class MoviesListViewController: UIViewController {
+        
+    // MARK: Dependency
+    
+    private var viewModel: MoviesListViewModel
+    private var imageRepository: ImageRepository
+
+    init(viewModel: MoviesListViewModel, imageRepository: ImageRepository) {
+        
+        self.viewModel = viewModel
+        self.imageRepository = imageRepository
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Storyboard are a pain")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
