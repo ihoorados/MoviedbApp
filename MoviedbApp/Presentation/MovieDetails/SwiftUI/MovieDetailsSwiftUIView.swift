542ec1f35fd5ac62cc428f10d93c4c116d7ad6a3
Hoorad <hooradramezani@gmail.com>
2025-01-18 23:05:55 +0330

    add SwiftUIView and ViewModel File

---
+++ MoviedbApp/Presentation/MovieDetails/SwiftUI/MovieDetailsSwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailsSwiftUIView.swift
+//  MoviedbApp
+//
+//  Created by Hoorad on 1/18/25.
+//
+
+import SwiftUI
+
+struct MovieDetailsSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailsSwiftUIView()
+}
